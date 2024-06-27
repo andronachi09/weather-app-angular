71885fd3732e9aac07f2f17efa6658d7a6a51525
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2024-06-27 15:31:06 +0200

    feat: Service search current weather

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SearchCity } from '../search-city';
+import { CurrentWeather } from '../current-weather';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class WeatherService {
   private locationsUrl = 'http://localhost:8080/locations';
+  private currentWeatherUrl = 'http://localhost:8080/weather';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-   searchLocations(query: string): Observable<SearchCity[]> {
-    return this.http.get<SearchCity[]>(`${this.locationsUrl}?q=${query}`)
+  searchLocations(query: string): Observable<SearchCity[]> {
+    return this.http.get<SearchCity[]>(`${this.locationsUrl}?q=${query}`);
+  }
+
+  searchCurrentWeather(lat: number, lon: number): Observable<CurrentWeather> {
+    return this.http.get<CurrentWeather>(`${this.currentWeatherUrl}?lat=${lat}&lon=${lon}`)
   }
 }
