1c025444cbc51e4216b38b49a7b11716eb902ede
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2024-06-27 15:31:37 +0200

    feat: route config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SearchCity } from './search-city';
-import { WeatherService } from './services/weather.service';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
     <main>
-      <app-search-bar></app-search-bar>
+      <router-outlet></router-outlet>
     </main>
   `,
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, FormsModule, CommonModule, SearchBarComponent],
+  imports: [RouterOutlet],
 })
-export class AppComponent {
-  title = 'weather-app';
-  locations: SearchCity[] = [];
-  searchQuery: string = '';
-
-  constructor(private weatherService: WeatherService) {}
-
-
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { WeatherDetailsComponent } from './weather-details/weather-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainPageComponent,
+        title: 'Home page'
+    },
+    {
+        path: 'details/:lat/:lon',
+        component: WeatherDetailsComponent,
+        title: 'Details Page'
+    }
+];
